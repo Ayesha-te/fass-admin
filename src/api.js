eab483dedc0a88eed4343d6fc5d1e11b7f373139
Ayesha-te <ayeshajahangirrr@gmail.com>
2026-04-07 21:03:09 +0500

    dsf

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://backend-13lk.onrender.com/api';
 const inflightRequests = new Map();
 
 function buildRequestKey(path, options = {}, token) {
